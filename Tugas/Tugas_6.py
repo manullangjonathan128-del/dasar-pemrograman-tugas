7ac00dc0d5d409cb732dbb780931e4446c742b5a
Henry Nugroho <manullangjonathan128@gmail.com>
2025-11-06 13:40:58 +0700

    Update Tugas_6.py

--- Tugas/Tugas_6.py
+++ Tugas/Tugas_6.py
@@ -1,14 +1,14 @@
-data = [[1, 2, 3],[4, 5, 6],[7, 8, 9],[0]
+angka = [[1, 2, 3],[4, 5, 6],[7, 8, 9],[0]
 ]
 
 print("Hasil")
 print("Baris pertama, Kolom pertama adalah :")
-print(data[0][0]) 
+print(angka[0][0]) 
 print("Baris pertama, Kolom kedua adalah :")
-print(data[0][1])
+print(angka[0][1])
 print("Baris pertama, Kolom ketiga adalah :")
-print(data[0][2])
+print(angka[0][2])
 print("Baris ketiga, Kolom ketiga adalah :")
-print(data[2][2])
+print(angka[2][2])
 print("Baris ke empat, kolom pertama adalah :")
 print(data[3][0])
