ec7446597bf408c3eb7d112866227ac0dadad05a
Henry Nugroho <manullangjonathan128@gmail.com>
2025-11-06 13:14:51 +0700

    Update Tugas_6.py

--- Tugas/Tugas_6.py
+++ Tugas/Tugas_6.py
@@ -1,20 +1,14 @@
-# Membuat List 2 Dimensi (Array)
-data = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-    [0]
+data = [[1, 2, 3],[4, 5, 6],[7, 8, 9],[0]
 ]
 
-# Menampilkan hasil
 print("Hasil")
-print("Baris Pertama, Kolom Pertama adalah :")
+print("Baris pertama, Kolom pertama adalah :")
 print(data[0][0]) # 1
-print("Baris Pertama, Kolom Kedua adalah :")
+print("Baris Pertama, Kolom kedua adalah :")
 print(data[0][1]) # 2
-print("Baris Pertama, Kolom Ketiga adalah :")
+print("Baris Pertama, Kolom ketiga adalah :")
 print(data[0][2]) # 3
-print("Baris Ketiga, Kolom Ketiga adalah :")
+print("Baris ketiga, Kolom ketiga adalah :")
 print(data[2][2]) # 9
 print("Baris Ke Empat, Kolom Pertama adalah :")
 print(data[3][0]) # 0
